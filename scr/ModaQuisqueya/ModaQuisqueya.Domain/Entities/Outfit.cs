b5cb485d27c904c99373da71fb084582b5a8eb2b
fairybugs <gilennimariel@gmail.com>
2025-07-25 23:58:32 -0500

    Agregando Infrastructure/Exceptions
    Probando la api en postman y agregando los exceptions.

--- scr/ModaQuisqueya/ModaQuisqueya.Domain/Entities/Outfit.cs
+++ scr/ModaQuisqueya/ModaQuisqueya.Domain/Entities/Outfit.cs
@@ -7,7 +7,6 @@ namespace ModaQuisqueya.Domain.Entities
         public int Id { get; set; }
         public string Nombre { get; set; } = null!;
         public string Descripcion { get; set; } = null!;
-        public string Ocasion { get; set; } = null!;
         public string ImagenUrl { get; set; } = null!;
     }
 }
---
+++ scr/ModaQuisqueya/ModaQusiqueya.Infrastructure/Exceptions/TendenciaNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModaQuisqueya.Infrastructure.Exceptions
+{
+    public class TendenciaNotFoundException : Exception
+    {
+        public TendenciaNotFoundException(int id)
+            : base($"La tendencia con ID {id} no fue encontrada.")
+        {
+        }
+    }
+}
